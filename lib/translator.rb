faa66efb369fe8fc36b09affe687427cbb2adfc4
Daniel Calderon <danieleduardocald@gmail.com>
2020-12-03 20:31:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -37,6 +37,7 @@ def get_japanese_emoticon (file_path, character)
 translation_hash = load_library(file_path)
 
 translation = []
+translation_2 = []
 #binding.pry
 translation_hash.each {|key,symbol|
   symbol.each {|inner_symbol, emoticon|
